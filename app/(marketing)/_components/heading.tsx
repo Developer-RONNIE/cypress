6db499c7d3aee065d93d214a47c70d36a8223a86
Developer-RONNIE <ronnieworkspace@gmail.com>
2024-06-18 22:14:17 +0530

    authication-clerk & DB-convex

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,17 @@
 "use client"
 
+import { useConvexAuth } from "convex/react";
+import { ArrowRight } from "lucide-react";
+import { SignInButton } from "@clerk/clerk-react";
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button"
-import { ArrowRight } from "lucide-react"
+import { Spinner } from "@/components/spinner";
+
 
 export const Heading = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
+
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -12,10 +20,27 @@ export const Heading = () => {
             <h3 className="text-base sm:text-xl md:text-2xl font-medium">
                 Cypress is the connected workspace where <br /> better, faster work happens.
             </h3>
-            <Button>
-                Enter Cypress
-                <ArrowRight className="h-4 w-4 ml-2" />
-            </Button>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size="lg"/>
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                <Link href="/documents">
+                    Enter Cypress
+                    <ArrowRight className="h-4 w-4 ml-2" />
+                </Link>
+                </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get Cypress free
+                        <ArrowRight className="h-4 w-4 ml-2"/>
+                    </Button>
+                </SignInButton>
+            )}
         </div>
     )
 }
