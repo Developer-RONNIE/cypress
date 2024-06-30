0594911b58c05848082c5a6d66156b144a6dd552
Developer-RONNIE <ronnieworkspace@gmail.com>
2024-06-30 18:05:35 +0530

    Banner component for trash + Menu

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -7,6 +7,8 @@ import { MenuIcon } from "lucide-react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { Title } from "./title";
+import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 
 interface Navbarprops {
@@ -26,8 +28,11 @@ export const Navbar = ({
 
     if (document === undefined){
         return (
-            <nav className="bg-background dark:bg-[#1F1F1F] px-3  py-2 w-full flex items-center" >
+            <nav className="bg-background dark:bg-[#1F1F1F] px-3  py-2 w-full flex items-center justify-between" >
                 <Title.Skeleton />
+                <div className="flex items-center gap-x-2">
+                    <Menu.Skeleton />
+                </div>
             </nav>
         )
     }
@@ -48,8 +53,14 @@ export const Navbar = ({
                  )}
                  <div className="flex items-center justify-between w-full">
                     <Title initialData={document} />
+                    <div className="flex items-center gap-x-2">
+                        <Menu documentId={document._id} />
+                    </div>
                  </div>
            </nav>
+           {document.isArchived && (
+            <Banner documentId={document._id} />
+           )}
         </>
     )
 }
